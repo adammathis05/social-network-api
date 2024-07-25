cdfe296057354f44a468ea30cc3246134be29fa0
Adam Mathis <adam.mathis05@gmail.com>
2024-07-25 14:52:23 -0500

    models updated

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,2 +1,34 @@
-const { Schema, model } = require('mongoose');
+const { Schema, Types } = require("mongoose");
+const dayJs = require("dayjs");
 
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      require: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      get: (ts) => dayJs(ts).format("MM/DD/YYYY"),
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+    _id: false,
+  }
+);
+
+module.exports = reactionSchema;
--- models/Thought.js
+++ models/Thought.js
@@ -1,2 +1,38 @@
 const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+const dayJs = require('dayjs');
 
+const thoughtSchema = new Schema(
+    {
+      thoughtText: {
+        type: String,
+        required: true,
+        minlength: 1, maxlength: 280,
+      },
+      username: {
+        type: String,
+        require: true,
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now(),
+        get: ts => dayJs(ts).format("MM/DD/YYYY")
+      },
+      reactions: [
+        reactionSchema
+      ],
+    },
+    {
+      toJSON: {
+        getters: true,
+      },
+      id: false,
+    }
+  );
+  thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+});
+  
+  const Thought = model('thought', thoughtSchema);
+  
+  module.exports = Thought;
